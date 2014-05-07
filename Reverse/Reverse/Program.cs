c1512ea4d3c82010ff7c6e20cfe46c9a85abec81
BCorr17 <CorrBradley@gmail.com>
2014-05-07 09:49:33 -0400

    hmzfmhfzhmfz

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse
+{
+    class Program
+    {
+        public static string Reverse(string input)
+        {
+            string output = "";
+
+            char[] seq = input.ToCharArray();
+
+            Array.Reverse(seq);
+
+            string final = new string(seq);
+            input = final;
+            return input;
+
+            //return output;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string:");
+
+            string input = Console.ReadLine();
+            string output = Reverse(input);
+            //string output = Reverse(Console.ReadLine));
+
+            Console.WriteLine(output);
+        }
+    }
+}
